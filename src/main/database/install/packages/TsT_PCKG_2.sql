10cf76ae4f0da55d0f1e697273570e8202fc21d7
h4d4p <60484485+h4d4p@users.noreply.github.com>
2020-02-17 02:55:18 +0300

    test

--- src/main/database/install/packages/TsT_PCKG_2.sql
+++ src/main/database/install/packages/TsT_PCKG_2.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 --validCheckSum: ANY
---changeset author: id1 dbms: oracle runOnChange: true splitStatements: false 
+--changeset author: id1 dbms: oracle runOnChange: true
 
 CREATE OR REPLACE PACKAGE TsT_PCG_2 AS
 
