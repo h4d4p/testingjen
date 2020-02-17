82a2f3e382a6f2d40cf90cd5560fc22eaf4892b8
ali3lo <32849607+ali3lo@users.noreply.github.com>
2020-02-18 00:19:46 +0300

    Update test_view.sql

--- src/main/database/install/views/test_view.sql
+++ src/main/database/install/views/test_view.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 --changeset author:test_view  runOnChange:true stripComments:false
 CREATE OR REPLACE VIEW test_view as
-	select 1 as a , 2 b, 3 c
+	elect 1 as a , 2 b, 3 c
 	from dual;
 	
